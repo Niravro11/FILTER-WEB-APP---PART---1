a153e64e70571b52967aeaa28bb343aa1b2b4f9a
Niravro11 <75479734+Niravro11@users.noreply.github.com>
2021-05-06 16:37:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX= 0;
+noseY= 0;
 function preload() {
-
+    clown_nose= loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 function setup () {
     canvas= createCanvas(300,300);
@@ -10,20 +12,22 @@ function setup () {
     poseNet=ml5.poseNet(video,modelLoaded);
     poseNet.on('pose',gotPoses);
 }
-function draw () {
-    image(video,0,0,300,300);
-}
-function take_snapshot () {
-    save('my filtered image.png');
-}
 function modelLoaded() {
-    console.log('modelLoaded!');
+    console.log('Pose Net is initialized');
     
 }
 function gotPoses(results) {
     if(results.length>0) {
         console.log(results);
-        console.log("Nose X =" + results[0].pose.nose.x );
-        console.log("Nose Y =" + results[0].pose.nose.y );
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-15;
     }
 }
+function draw () {
+    image(video,0,0,300,300);
+    image(clown_nose,noseX,noseY,30,30);
+}
+function take_snapshot () {
+    save('My filtered image.png');
+}
+
